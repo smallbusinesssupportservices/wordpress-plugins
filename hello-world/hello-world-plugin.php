3558f36e39754fd30e29c52cc7005a0f86603890
William Brewer <william@smallbusinessessupport.services>
2024-10-31 06:07:50 -0400

    hello world

---
+++ hello-world/hello-world-plugin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World in PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello, World!";
+        ?>
+    </h1>
+</body>
+</html>
